252ccf68d62eb00148e265308114e7df4e8bd26d
Ryoneme2 <worknarair6@gmail.com>
2022-02-19 16:42:26 +0700

    change config

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,3 +1,3 @@
 <?php
 //mysql://b3d120d597b92b:445d9ae4@us-cdbr-east-05.cleardb.net/heroku_384b93e1b8094ab?reconnect=true
-$con = mysqli_connect("us-cdbr-east-05.cleardb.net", "b3d120d597b92b", "445d9ae4", "heroku_384b93e1b8094ab") or die("Connect ERROR");
+$con = mysqli_connect("localhost", "root", "", "achats") or die("Connect ERROR");
