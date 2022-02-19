a1bb462b26ad4ced042a58d7c42bd90dc8865ceb
Ryoneme2 <worknarair6@gmail.com>
2022-02-19 22:43:06 +0700

    fix del product

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,3 +1,3 @@
 <?php
-//mysql://b3d120d597b92b:445d9ae4@us-cdbr-east-05.cleardb.net/heroku_384b93e1b8094ab?reconnect=true
+
 $con = mysqli_connect("localhost", "root", "", "achats") or die("Connect ERROR");
--- service/delProduct.php
+++ service/delProduct.php
@@ -2,24 +2,24 @@
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <?php
+session_start();
 require_once '../config/dbcon.php';
 
 $id = $_GET['id'];
-session_start();
 $idSeller = $_SESSION['seller_id'];
 
-$sqlSellCmd = "SELECT seller_product_qty FROM seller_info WHERE seller_id = $id";
+$sqlSellCmd = "SELECT seller_product_qty FROM seller_info WHERE seller_id = $idSeller";
 
 // echo $sqlCmd;
 $result2 = mysqli_query($con, $sqlSellCmd);
-$row = mysqli_fetch_array($result2);
-$qty = $row['seller_product_qty '];
+$row = mysqli_fetch_assoc($result2);
+$qty = $row['seller_product_qty'];
 $qty2 = $qty - 1;
 
 $sqlSellQtyCmd = "UPDATE seller_info SET seller_product_qty = $qty2 WHERE seller_id = $idSeller";
 
 
-$sql = 'DELETE FROM product WHERE prod_id =' . $idSeller;
+$sql = 'DELETE FROM product WHERE prod_id =' . $id;
 
 $result = mysqli_query($con, $sql);
 $result3 = mysqli_query($con, $sqlSellQtyCmd);
